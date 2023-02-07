71264cfbaa8644c5e3b3e8a22eae2a0a806c0adb
fturello <92311869+fturello@users.noreply.github.com>
2023-02-07 17:03:53 +0100

    before components

---
+++ src/components/NotAvailable.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../styles/NotAvailable.css";
+
+function NotAvailable() {
+	return (
+		<div className='not-available'>
+			This screen resolution is not supported yet.
+		</div>
+	);
+}
+
+export default NotAvailable;
